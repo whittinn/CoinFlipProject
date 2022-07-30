1601f43596447ead626ae08452398c8860a2a17c
Nathaniel Whittington <whittinn@gmail.com>
2022-07-29 19:05:08 -0500

    Initial Commit

---
+++ CoinFlipProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinFlipProject
+//
+//  Created by Nathaniel Whittington on 7/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
